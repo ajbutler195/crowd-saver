509342fd309e74699d7f48c188e262f72e90c230
Allie Davenport <alliedavenport@Allies-MacBook-Pro.local>
2024-03-02 00:17:59 -0500

    whats up aj

---
+++ simulation.py
@@ -0,0 +1,17 @@
+from dot import Person
+
+def run_simulation(people):
+    for person in people:
+        pass
+
+def generate_person() -> Person:
+    pass
+
+def generate_people():
+    pass
+
+
+
+if __name__ == "__main__":
+    people = generate_people()
+    run_simulation(people)
